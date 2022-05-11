ffdba7a85f215759932c7e4fb3d7e81b7647b0b7
gaurav7477 <bairagi7477@gmail.com>
2022-05-11 11:22:45 +0530

    first commit

---
+++ pattern1.cpp
@@ -0,0 +1,135 @@
+#include<iostream>
+#include<bits/c++io.h>
+
+using namespace std;
+
+int main(){
+
+// Rectangle Pettern
+    // int row,column;
+    // cin>> row >> column;
+
+    // for (int i = 0; i < row; i++)
+    // {
+    //     for (int j = 0; j < column; j++)
+    //     {
+    //         cout<< "*";
+
+    //     }
+    //     cout<< endl;
+    // }
+    
+
+// Hollow Rectangle Pettern
+    // int row,column;
+    // cin>> row >> column;
+
+    // for (int i = 0; i < row; i++)
+    // {
+    //     for (int j = 0; j < column; j++)
+    //     {
+    //         if(i == 0  || i == row-1 || j == 0 || j == column-1){
+
+    //             cout << "*";
+    //         }
+    //         else{
+    //             cout<< " ";
+    //         }
+           
+    //     }
+    //  cout<< endl;
+    // }
+
+
+
+
+ // inverted half pramid
+    
+    // int n;
+    // cin>>n;
+    // for(int i = 1;i <= n;i++){
+    //     for (int j = 1; j <=n-i; j++)
+    //     {
+    //         std::cout << "*";
+
+    //     }
+    //     cout<<endl;
+        
+    // }
+
+
+
+// half pramid
+
+    // int n;
+    // cin>>n;
+    // for(int i = 0;i < n;i++){
+    //     for (int j = 0; j < i; j++)
+    //     {
+    //         cout << "*";
+    //     }
+    //     cout<<endl;
+        
+    // }
+
+// half pramid 180 degree rotation
+
+//    int n;
+//     cin>>n;
+//     for(int i = 0; i < n;i++){
+//         for (int j = 0; j < n; j++)
+//         {
+//             if(j < n-i){
+//                 cout<< " ";
+//             }
+//             else{
+//                 cout << "*";
+//             }
+            
+//         }
+//         cout<<endl;
+        
+//     }
+
+// half pramid using number 
+
+    //                                         1
+    //                                         22
+    //                                         333
+    //                                         4444
+    //                                         55555   
+    // int n;                                        
+    // cin>>n;
+    // for(int i = 0;i < n;i++){
+    //     for (int j = 0; j < i; j++)
+    //     {
+    //         cout << i;
+    //     }
+    //     cout<<endl;
+        
+    // }
+
+
+// floyd`s trangle
+// 1 
+// 2 3 
+// 4 5 6 
+// 7 8 9 10 
+// 11 12 13 14 15 
+    // int n;
+    // int count = 1;
+    // cin>>n;
+    // for(int i = 0;i < n;i++){
+    //     for (int j = 0; j < i; j++)
+    //     {
+    //         cout << count<< " ";
+    //         count++;
+    //     }
+    //     cout<<endl;
+        
+        
+    // }
+
+// butterfly pattern
+                                      
+}
